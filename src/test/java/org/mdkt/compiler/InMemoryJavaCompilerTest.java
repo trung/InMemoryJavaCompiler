e10c16859b2c9f55635cca6ca7fc31912eead7cc
turpid-monkey <s1496737@stud.uni-frankfurt.de>
2015-09-25 16:14:40 +0200

    Added support for compiling multiple, dependent classes

--- src/test/java/org/mdkt/compiler/InMemoryJavaCompilerTest.java
+++ src/test/java/org/mdkt/compiler/InMemoryJavaCompilerTest.java
@@ -1,5 +1,8 @@
 package org.mdkt.compiler;
 
+import java.io.StringWriter;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -20,5 +23,23 @@ public class InMemoryJavaCompilerTest {
         Class<?> helloClass = InMemoryJavaCompiler.compile("org.mdkt.HelloClass", sourceCode.toString());
         Assert.assertNotNull(helloClass);
         Assert.assertEquals(1, helloClass.getDeclaredMethods().length);
+    }
+    
+    @Test
+    public void compile_severalFiles() throws Exception {
+    	String cls1 = "public class A{ public B b() { return new B(); }}";
+        String cls2 = "public class B{ public String toString() { return \"B!\"; }}";
+        
+        InMemoryJavaCompiler compiler = new InMemoryJavaCompiler();
+        compiler.addSource("A", cls1);
+        compiler.addSource("B", cls2);
+        Map<String,Class<?>> compiled = compiler.compileAll();
+        ;
+        Assert.assertNotNull(compiled.get("A"));
+        Assert.assertNotNull(compiled.get("B"));
+
+        Class<?> aClass = compiled.get("A");
+        Object a = aClass.newInstance();
+        Assert.assertEquals("B!", aClass.getMethod("b").invoke(a).toString());
     }
 }
