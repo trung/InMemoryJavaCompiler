96b17fef2e11ceccd9692d47656462c019bbf72e
PKeidel <pkeidel@sags-per-mail.de>
2015-10-13 22:24:58 +0200

    Added Support for CompileException to get notified when an error occures

---
+++ src/main/java/org/mdkt/compiler/CompileException.java
@@ -0,0 +1,10 @@
+package org.mdkt.compiler;
+
+/**
+ * Created by PKeidel on 13.10.15.
+ */
+public class CompileException extends Exception {
+    public CompileException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/org/mdkt/compiler/InMemoryJavaCompiler.java
+++ src/main/java/org/mdkt/compiler/InMemoryJavaCompiler.java
@@ -1,24 +1,39 @@
 package org.mdkt.compiler;
 
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.ToolProvider;
+import javax.tools.*;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by trung on 5/3/15.
+ * Changed by PKeidel on 13.10.15.
  */
 public class InMemoryJavaCompiler {
     static JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
 
     public static Class<?> compile(String className, String sourceCodeInText) throws Exception {
+        final DiagnosticCollector<JavaFileObject> diagnosticsCollector = new DiagnosticCollector<>();
         SourceCode sourceCode = new SourceCode(className, sourceCodeInText);
         CompiledCode compiledCode = new CompiledCode(className);
-        Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(sourceCode);
+        Iterable<? extends JavaFileObject> compilationUnits = Collections.singletonList(sourceCode);
         DynamicClassLoader cl = new DynamicClassLoader(ClassLoader.getSystemClassLoader());
-        ExtendedStandardJavaFileManager fileManager = new ExtendedStandardJavaFileManager(javac.getStandardFileManager(null, null, null), compiledCode, cl);
-        JavaCompiler.CompilationTask task = javac.getTask(null, fileManager, null, null, null, compilationUnits);
+        ExtendedStandardJavaFileManager fileManager = new ExtendedStandardJavaFileManager(javac.getStandardFileManager(diagnosticsCollector, null, null), compiledCode, cl);
+        JavaCompiler.CompilationTask task = javac.getTask(null, fileManager, diagnosticsCollector, null, null, compilationUnits);
         boolean result = task.call();
+        fileManager.close();
+
+        if (!result) {
+            StringBuilder sb = new StringBuilder();
+            List<Diagnostic<? extends JavaFileObject>> diagnostics = diagnosticsCollector.getDiagnostics();
+            for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics) {
+                // read error dertails from the diagnostic object
+                sb.append("=> ").append(diagnostic.getMessage(null)).append("\n");
+            }
+            throw new CompileException(sb.toString());
+        }
+
         return cl.loadClass(className);
     }
 }
