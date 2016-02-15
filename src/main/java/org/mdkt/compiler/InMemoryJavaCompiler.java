30bf60f0c421e8c6e3acd68ca57983359c7c9bd6
Ryan <ryanf.rowe@gmail.com>
2016-02-15 00:54:06 -0800

    Allow for diagnostics when compiling

--- src/main/java/org/mdkt/compiler/InMemoryJavaCompiler.java
+++ src/main/java/org/mdkt/compiler/InMemoryJavaCompiler.java
@@ -1,24 +1,34 @@
 package org.mdkt.compiler;
 
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.ToolProvider;
-import java.util.Arrays;
+import javax.tools.*;
+import java.util.*;
 
 /**
  * Created by trung on 5/3/15.
  */
 public class InMemoryJavaCompiler {
-    static JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
+    static  JavaCompiler                        javac = ToolProvider.getSystemJavaCompiler();
+    private DiagnosticCollector<JavaFileObject> collector;
+    private boolean result;
 
-    public static Class<?> compile(String className, String sourceCodeInText) throws Exception {
+    public Class<?> compile(String className, String sourceCodeInText) throws Exception {
+        collector = new DiagnosticCollector<>();
         SourceCode sourceCode = new SourceCode(className, sourceCodeInText);
         CompiledCode compiledCode = new CompiledCode(className);
-        Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(sourceCode);
+        Iterable<? extends JavaFileObject> compilationUnits = Collections.singletonList(sourceCode);
         DynamicClassLoader cl = new DynamicClassLoader(ClassLoader.getSystemClassLoader());
         ExtendedStandardJavaFileManager fileManager = new ExtendedStandardJavaFileManager(javac.getStandardFileManager(null, null, null), compiledCode, cl);
-        JavaCompiler.CompilationTask task = javac.getTask(null, fileManager, null, null, null, compilationUnits);
-        boolean result = task.call();
+        JavaCompiler.CompilationTask task = javac.getTask(null, fileManager, collector, null,
+                                                          null, compilationUnits);
+        result = task.call();
         return cl.loadClass(className);
+    }
+
+    public boolean getResult() {
+        return result;
+    }
+
+    public List<Diagnostic<? extends JavaFileObject>> getDiagnostics() {
+        return collector.getDiagnostics();
     }
 }
