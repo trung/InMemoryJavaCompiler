ac95c0af938346bb0ef1a332f8483fb26bfd21e6
Ryan <ryanf.rowe@gmail.com>
2016-02-15 12:38:12 -0800

    Added diagnostic exception

---
+++ src/main/java/org/mdkt/compiler/InMemoryCompilerException.java
@@ -0,0 +1,50 @@
+package org.mdkt.compiler;
+
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.util.*;
+
+/**************************************************
+ * @author Ryan Rowe (1531352)
+ *         2/15/16
+ *         CSE 143 B/BP
+ *         TA: Chloe Lathe
+ *         InMemoryJavaCompiler
+ *         <p/>
+ *         Description
+ **************************************************/
+
+public class InMemoryCompilerException extends Exception {
+
+    private List<Diagnostic<? extends  JavaFileObject>> diags;
+    private int line;
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public InMemoryCompilerException(List<Diagnostic<? extends JavaFileObject>> diags) {
+        this.diags = diags;
+        line = -1;
+    }
+
+    public List<Map<String, Object>> getErrorList() {
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        for(Diagnostic diag : diags) {
+            Map<String, Object> diagnostic = new HashMap<>();
+
+            diagnostic.put("type", diag.getCode());
+            diagnostic.put("line", diag.getLineNumber() - line + 1);
+            diagnostic.put("message", diag.getMessage(Locale.US));
+
+            list.add(diagnostic);
+        }
+
+        return list;
+    }
+
+    public void setInsertLine(int line) {
+        this.line = line;
+    }
+}
