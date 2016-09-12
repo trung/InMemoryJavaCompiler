ff33429b9a95f42dddd09dc232e5996751063fb8
Muhammad Hammad <hammad@172.16.4.240>
2016-09-12 19:33:59 +0200

    Merged all pull requests

--- src/main/java/org/mdkt/compiler/InMemoryJavaCompiler.java
+++ src/main/java/org/mdkt/compiler/InMemoryJavaCompiler.java
@@ -1,24 +1,71 @@
 package org.mdkt.compiler;
 
+import javax.tools.DiagnosticCollector;
 import javax.tools.JavaCompiler;
 import javax.tools.JavaFileObject;
 import javax.tools.ToolProvider;
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by trung on 5/3/15.
  */
 public class InMemoryJavaCompiler {
-    static JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
+    private static final Iterable<String> options = Collections.singletonList("-Xlint:unchecked");
+    private static JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
+    DynamicClassLoader classLoader = new DynamicClassLoader(ClassLoader.getSystemClassLoader());
+    private DiagnosticCollector<JavaFileObject> collector;
 
-    public static Class<?> compile(String className, String sourceCodeInText) throws Exception {
-        SourceCode sourceCode = new SourceCode(className, sourceCodeInText);
-        CompiledCode compiledCode = new CompiledCode(className);
-        Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(sourceCode);
-        DynamicClassLoader cl = new DynamicClassLoader(ClassLoader.getSystemClassLoader());
-        ExtendedStandardJavaFileManager fileManager = new ExtendedStandardJavaFileManager(javac.getStandardFileManager(null, null, null), compiledCode, cl);
-        JavaCompiler.CompilationTask task = javac.getTask(null, fileManager, null, null, null, compilationUnits);
-        boolean result = task.call();
-        return cl.loadClass(className);
+    public InMemoryJavaCompiler() {
+        this.collector = new DiagnosticCollector<>();
     }
+
+    Map<String, SourceCode> clazzCode = new HashMap<>();
+
+    public void addSource(String className, String sourceCodeInText)
+            throws Exception {
+        clazzCode.put(className, new SourceCode(className, sourceCodeInText));
+    }
+
+    public Map<String, Class<?>> compileAll() throws Exception {
+
+        Collection<SourceCode> compilationUnits = clazzCode.values();
+        List<CompiledCode> compiledCodes = new ArrayList<>();
+        Iterator<SourceCode> iterator = compilationUnits.iterator();
+        for (SourceCode sourceCode : compilationUnits) {
+            compiledCodes.add(new CompiledCode(sourceCode.getClassName()));
+        }
+
+        ExtendedStandardJavaFileManager fileManager = new ExtendedStandardJavaFileManager(javac.getStandardFileManager(null, null, null), compiledCodes, classLoader);
+
+        JavaCompiler.CompilationTask task = javac.getTask(null, fileManager, collector,
+                options, null, compilationUnits);
+
+
+        try {
+            boolean result = task.call();
+
+            if (!result || collector.getDiagnostics().size() > 0) {
+                throw new InMemoryCompilerException(collector.getDiagnostics());
+            }
+
+            Map<String, Class<?>> classes = new HashMap<String, Class<?>>();
+            for (String className : clazzCode.keySet()) {
+                classes.put(className, classLoader.loadClass(className));
+            }
+            return classes;
+        } catch (ClassFormatError e) {
+            throw new InMemoryCompilerException(collector.getDiagnostics());
+        }
+
+    }
+
+
+    public Class<?> compile(String className, String sourceCodeInText) throws Exception {
+        addSource(className, sourceCodeInText);
+        Map<String, Class<?>> compiled = compileAll();
+        Class<?> compiledClass = compiled.get(className);
+        return compiledClass;
+    }
+
+
 }
--- src/test/java/org/mdkt/compiler/InMemoryJavaCompilerTest.java
+++ src/test/java/org/mdkt/compiler/InMemoryJavaCompilerTest.java
@@ -3,6 +3,8 @@ package org.mdkt.compiler;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Map;
+
 /**
  * Created by trung on 5/3/15.
  */
@@ -10,14 +12,51 @@ public class InMemoryJavaCompilerTest {
 
     @Test
     public void compile_whenTypical() throws Exception {
-        StringBuffer sourceCode = new StringBuffer();
+        StringBuilder sourceCode = new StringBuilder();
 
         sourceCode.append("package org.mdkt;\n");
         sourceCode.append("public class HelloClass {\n");
         sourceCode.append("   public String hello() { return \"hello\"; }");
         sourceCode.append("}");
 
-        Class<?> helloClass = InMemoryJavaCompiler.compile("org.mdkt.HelloClass", sourceCode.toString());
+        InMemoryJavaCompiler inMemoryJavaCompiler = new InMemoryJavaCompiler();
+        Class<?> helloClass = inMemoryJavaCompiler.compile("org.mdkt.HelloClass", sourceCode.toString());
+        Assert.assertNotNull(helloClass);
+        Assert.assertEquals(1, helloClass.getDeclaredMethods().length);
+    }
+
+
+    @Test
+    public void compile_severalFiles() throws Exception {
+        String cls1 = "public class A{ public B b() { return new B(); }}";
+        String cls2 = "public class B{ public String toString() { return \"B!\"; }}";
+
+        InMemoryJavaCompiler compiler = new InMemoryJavaCompiler();
+        compiler.addSource("A", cls1);
+        compiler.addSource("B", cls2);
+        Map<String, Class<?>> compiled = compiler.compileAll();
+        ;
+        Assert.assertNotNull(compiled.get("A"));
+        Assert.assertNotNull(compiled.get("B"));
+
+        Class<?> aClass = compiled.get("A");
+        Object a = aClass.newInstance();
+        Assert.assertEquals("B!", aClass.getMethod("b").invoke(a).toString());
+    }
+
+
+    @Test
+    public void compile_filesWithInnerClasses() throws Exception {
+        StringBuffer sourceCode = new StringBuffer();
+
+        sourceCode.append("package org.mdkt;\n");
+        sourceCode.append("public class HelloClass {\n");
+        sourceCode.append("   private static class InnerHelloWorld { int inner; }\n");
+        sourceCode.append("   public String hello() { return \"hello\"; }");
+        sourceCode.append("}");
+
+        InMemoryJavaCompiler inMemoryJavaCompiler = new InMemoryJavaCompiler();
+        Class<?> helloClass = inMemoryJavaCompiler.compile("org.mdkt.HelloClass", sourceCode.toString());
         Assert.assertNotNull(helloClass);
         Assert.assertEquals(1, helloClass.getDeclaredMethods().length);
     }
